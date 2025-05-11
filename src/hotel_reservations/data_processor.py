913c0f843e6566114d92a75737ac54b4d1bc3b93
kirtikumarpandya <kirtikumar.pandya@uniper.energy>
2025-05-12 00:36:30 +0200

    Enhance DataProcessor: add data splitting and saving methods, and implement column renaming utility

--- src/hotel_reservations/data_processor.py
+++ src/hotel_reservations/data_processor.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import current_timestamp, to_utc_timestamp
+from sklearn.model_selection import train_test_split
 
 from hotel_reservations.config import ProjectConfig
+from hotel_reservations.helper import rename_spark_df_column_name
 
 
 class DataProcessor:
@@ -60,4 +63,59 @@ class DataProcessor:
             + pandas_df["arrival_month"].astype(str).str.zfill(2)
             + "-"
             + pandas_df["arrival_date"].astype(str).str.zfill(2)
+        )
+
+        # Handle numeric features
+        num_features = self.config.num_features
+        for col in num_features:
+            self.df[col] = pd.to_numeric(self.df[col], errors="coerce")
+
+    def split_data(self, test_size: float = 0.2, random_state: int = 42) -> tuple[pd.DataFrame, pd.DataFrame]:
+        """Split the DataFrame (self.df) into training and test sets.
+
+        :param test_size: The proportion of the dataset to include in the test split.
+        :param random_state: Controls the shuffling applied to the data before applying the split.
+        :return: A tuple containing the training and test DataFrames.
+        """
+        train_set, test_set = train_test_split(self.df, test_size=test_size, random_state=random_state)
+        return train_set, test_set
+
+    def save_to_catalog(self, train_set: pd.DataFrame, test_set: pd.DataFrame) -> None:
+        """Save the train and test sets into Databricks tables.
+
+        :param train_set: The training DataFrame to be saved.
+        :param test_set: The test DataFrame to be saved.
+        """
+        train_set_with_timestamp = self.spark.createDataFrame(train_set).withColumn(
+            "update_timestamp_utc", to_utc_timestamp(current_timestamp(), "UTC")
+        )
+
+        test_set_with_timestamp = self.spark.createDataFrame(test_set).withColumn(
+            "update_timestamp_utc", to_utc_timestamp(current_timestamp(), "UTC")
+        )
+
+        train_set_with_timestamp = rename_spark_df_column_name(train_set_with_timestamp)
+        test_set_with_timestamp = rename_spark_df_column_name(test_set_with_timestamp)
+
+        train_set_with_timestamp.write.mode("overwrite").saveAsTable(
+            f"{self.config.catalog_name}.{self.config.schema_name}.train_set_hotel_reservations"
+        )
+
+        test_set_with_timestamp.write.mode("overwrite").saveAsTable(
+            f"{self.config.catalog_name}.{self.config.schema_name}.test_set_hotel_reservations"
+        )
+
+    def enable_change_data_feed(self) -> None:
+        """Enable Change Data Feed for train and test set tables.
+
+        This method alters the tables to enable Change Data Feed functionality.
+        """
+        self.spark.sql(
+            f"ALTER TABLE {self.config.catalog_name}.{self.config.schema_name}.train_set_hotel_reservations"
+            "SET TBLPROPERTIES (delta.enableChangeDataFeed = true);"
+        )
+
+        self.spark.sql(
+            f"ALTER TABLE {self.config.catalog_name}.{self.config.schema_name}.test_set_hotel_reservations"
+            "SET TBLPROPERTIES (delta.enableChangeDataFeed = true);"
         )

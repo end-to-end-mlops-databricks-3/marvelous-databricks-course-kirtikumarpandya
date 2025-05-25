daa9db605dfc80538ba1c9a31a415d3b4859042d
kirtikumarpandya <kirtikumar.pandya@uniper.energy>
2025-05-25 23:47:27 +0200

    fix: update expected columns in column selection test to include 'date'

--- tests/unit_tests/test_dataprocessor.py
+++ tests/unit_tests/test_dataprocessor.py
@@ -70,7 +70,9 @@ def test_column_selection(sample_data: pd.DataFrame, config: ProjectConfig, spar
     processor = DataProcessor(pandas_df=sample_data, config=config, spark=spark_session)
     processor.preprocess()
 
-    expected_columns = config.cat_features + config.num_features + [config.target, "Booking_ID"] + config.date_features
+    expected_columns = (
+        config.cat_features + config.num_features + [config.target, "Booking_ID", "date"] + config.date_features
+    )
     assert set(processor.pandas_df.columns) == set(expected_columns)
 
 
